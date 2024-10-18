5f002059b25a684eae1d35b9df99d3bdcd5047da
tcodes317 <tcodes317@gmail.com>
2024-10-18 01:32:36 -0700

    done

---
+++ app/pages/index.js
@@ -0,0 +1,70 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import HomePage from "./../src/Pages/HomePage";
+import SignUpPage from "./../src/Pages/SignUpPage";
+import LoginPage from "./../src/Pages/LoginPage";
+import AdminPage from "./../src/Pages/AdminPage";
+import CategoryPage from "./../src/Pages/CategoryPage";
+
+require("./../src/stores/useUserStore")
+
+import Navbar from "./../src/components/Navbar";
+import { Toaster } from "react-hot-toast";
+import { useUserStore } from "./../src/stores/useUserStore";
+import { useEffect } from "react";
+import LoadingSpinner from "./../src/components/LoadingSpinner";
+import CartPage from "./../src/Pages/CartPage";
+import { useCartStore } from "./../src/stores/useCartStore";
+import PurchaseSuccessPage from "./../src/Pages/PurchaseSuccessPage";
+import PurchaseCancelPage from "./../src/Pages/PurchaseCancelPage";
+
+export default function Home() {
+
+  
+	const { user, checkAuth, checkingAuth } = useUserStore();
+	const { getCartItems } = useCartStore();
+	useEffect(() => {
+		checkAuth();
+	}, [checkAuth]);
+
+	useEffect(() => {
+		if (!user) return;
+
+		getCartItems();
+	}, [getCartItems, user]);
+
+	if (checkingAuth) return <LoadingSpinner />;
+
+  return (
+    
+		<div className='min-h-screen bg-gray-900 text-white relative overflow-hidden'>
+    {/* Background gradient */}
+    <div className='absolute inset-0 overflow-hidden'>
+      <div className='absolute inset-0'>
+        <div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(1,185,119,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]' />
+      </div>
+    </div>
+
+    <div className='relative z-50 pt-20'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to='/' />} />
+        <Route path='/login' element={!user ? <LoginPage /> : <Navigate to='/' />} />
+        <Route
+          path='/secret-dashboard'
+          element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />}
+        />
+        <Route path='/category/:category' element={<CategoryPage />} />
+        <Route path='/cart' element={user ? <CartPage /> : <Navigate to='/login' />} />
+        <Route
+          path='/purchase-success'
+          element={user ? <PurchaseSuccessPage /> : <Navigate to='/login' />}
+        />
+        <Route path='/purchase-cancel' element={user ? <PurchaseCancelPage /> : <Navigate to='/login' />} />
+      </Routes>
+    </div>
+    <Toaster />
+  </div>
+  );
+}
